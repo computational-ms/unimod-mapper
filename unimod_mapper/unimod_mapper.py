70ca32cad19e8d42822e4073ce535201dff5875f
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2021-05-18 13:41:23 +0200

    * further tweaks around unimod.xml handling functionality

--- unimod_mapper/unimod_mapper.py
+++ unimod_mapper/unimod_mapper.py
@@ -23,10 +23,11 @@ import xml.etree.ElementTree as ET
 import requests
 
 from pathlib import Path
+from loguru import logger
 
-
+# define the url from where unimod.xml file should be retrieved
 url = "http://www.unimod.org/xml/unimod.xml"
-from pathlib import Path
+
 
 class UnimodMapper(object):
     """
@@ -47,8 +48,7 @@ class UnimodMapper(object):
         self._mapper = None
 
         # Check if unimod.xml file exists & if not reset refresh_xml flag
-        xml_file_name = os.path.basename(url)
-        full_path = Path(__file__).parent / xml_file_name
+        full_path = Path(__file__).parent / "unimod.xml"
         if os.path.exists(full_path) is False:
             refresh_xml = True
 
@@ -57,7 +57,6 @@ class UnimodMapper(object):
             with open(full_path, "wb") as file:
                 file.write(response.content)
 
-        self.unimod_xml_name = xml_file_name
         self.unimod_xml_names = ["unimod.xml", "usermods.xml"]
         # self.data_list = self._parseXML()
         # self.mapper    = self._initialize_mapper()
@@ -97,11 +96,10 @@ class UnimodMapper(object):
         #         )
         #     )
         data_list = []
-        # xml_names = [self.unimod_xml_name, "usermods.xml"]
         for xml_name in self.unimod_xml_names:
             xmlFile = Path(__file__).parent / xml_name
-            print("> Parsing mods file ({0})".format(xmlFile))
             if os.path.exists(xmlFile):
+                logger.info("> Parsing mods file ({0})".format(xmlFile))
                 unimodXML = ET.iterparse(
                     codecs.open(xmlFile, "r", encoding="utf8"), events=(b"start", b"end")
                 )
@@ -138,11 +136,14 @@ class UnimodMapper(object):
                         else:
                             pass
             else:
-                if xml_name == self.unimod_xml_name:
-                    print("No unimod.xml file found. Expected at {0}".format(xmlFile))
+                if xml_name == "unimod.xml":
+                    logger.warning("No unimod.xml file found. Expected at {0}".format(
+                        xmlFile))
+                    # at least unimod.xml HAS to be available!
                     sys.exit(1)
                 elif xml_name == "usermods.xml":
-                    print("No usermods.xml file found. Expected at {0}".format(xmlFile))
+                    logger.info("No usermods.xml file found. Expected at {0}".format(
+                        xmlFile))
                     continue
         return data_list
 
